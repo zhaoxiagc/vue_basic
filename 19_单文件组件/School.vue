f03b5d5f4af0c409b1bd35e0d128340a5f94e73f
赵霞 <18418127626@163.com>
2022-07-18 13:04:36 +0800

    Add files via upload

---
+++ 19_单文件组件/School.vue
@@ -0,0 +1,51 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2>学校名称：{{name}}</h2>
+        <h2>学校地址：{{address}}</h2>
+        <button @click="showName">点我提示学校名</button>
+    </div>
+</template>
+
+<script>
+    //组件交互相关的代码（数据、方法等）
+    //export const school = Vue.extend({    //分别暴露
+    /* const school = Vue.extend({
+        data(){
+            return {
+                name:'尚硅谷',
+                address:'北京昌平'
+            }
+        },
+        methods: {
+            showName(){
+                alert(this.schoolName)
+            }
+        },
+    })
+    //export{school}        //统一暴露
+    export default school   //默认暴露 */
+
+    //简化写法
+    export default {
+        name:'School',
+        data(){
+            return {
+                name:'尚硅谷',
+                address:'北京昌平'
+            }
+        },
+        methods: {
+            showName(){
+                alert(this.name)
+            }
+        },
+    }
+</script>
+
+<style>
+    /* 组件的样式 */
+    .demo{
+        background-color: orange;
+    }
+</style>
---
+++ 19_单文件组件/Student.vue
@@ -0,0 +1,26 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2>学生姓名：{{name}}</h2>
+        <h2>学生年龄：{{age}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Student',
+        data(){
+            return {
+                name:'张三',
+                age:18
+            }
+        }
+    }
+</script>
+
+<style>
+    /* 组件的样式 */
+    .demo{
+        background-color: orange;
+    }
+</style>
